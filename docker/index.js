49f592e614e31d34b5ecfd0f0f11296534534405
HeeMinKim <81614922+khmdev87@users.noreply.github.com>
2022-08-14 23:03:41 +0900

    Update index.js

--- docker/index.js
+++ docker/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require('express');
+
+const PORT = 3000;
+
+//APP 
 const app = express();
-const port = 3000;
+app.get('/', (req,res) => {
+    res.send("반가워요!!!!!!!")
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(PORT);
+console.log("Server is running")
